94ffe64405ddb00fa4b80ed8bbd19afe457de0b1
unknown <a5932016@gmail.com>
2020-07-25 17:53:34 +0800

    commit

---
+++ Chapter2-ServerClient/Client/Client/Client.cpp
@@ -0,0 +1,42 @@
+﻿// Client.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma comment(lib,"WS2_32")
+
+int _cdecl main(void)
+{
+	// 初始化
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+	SOCKET s = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	// 取得伺服器位置
+	sockaddr_in sockAddr;
+	sockAddr.sin_family = AF_INET;
+	sockAddr.sin_port = htons(827);
+	inet_pton(AF_INET, "127.0.0.1", (void*)&sockAddr.sin_addr.S_un.S_addr);
+
+	// 連接 Server
+	connect(s, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR));
+
+	// 傳送訊息
+	char szMessage[] = "Give me the money.";
+	send(s, szMessage, strlen(szMessage) + sizeof(char), 0);
+
+	// 接收資料
+	char szBuffer[MAXBYTE] = { 0 };
+	recv(s, szBuffer, MAXBYTE, NULL);
+	printf("szBuffer: %s \r\n", szBuffer);
+
+	// 關閉與連線
+	shutdown(s, 0);
+	closesocket(s);
+	WSACleanup();
+	system("pause");
+	return 0;
+}
---
+++ Chapter2-ServerClient/Server/Server/Server.cpp
@@ -0,0 +1,49 @@
+﻿// Server.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <winsock2.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma comment(lib,"WS2_32")
+
+int _cdecl main()
+{
+	// 初始化
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+	SOCKET s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	SOCKADDR_IN sockaddr;
+	sockaddr.sin_family = AF_INET;
+	sockaddr.sin_port = htons(827);
+	sockaddr.sin_addr.S_un.S_addr = INADDR_ANY;
+
+	// 綁定通訊阜
+	bind(s, (SOCKADDR*)&sockaddr, sizeof(SOCKADDR));
+	listen(s, 1);
+	SOCKADDR clientAddr;
+	int nSize = sizeof(SOCKADDR);
+	SOCKET clientSock;
+
+	// 取得與Client聯繫的socket
+	clientSock = accept(s, (SOCKADDR*)&clientAddr, &nSize);
+	char inbuf[1024];
+
+	// 接收資料
+	recv(clientSock, inbuf, sizeof(inbuf), NULL);
+	printf("Receive: %s\r\n", inbuf);
+	char outbuf[] = "Hello Polaris\r\n";
+
+	// 傳送資料
+	send(clientSock, outbuf, strlen(outbuf) + sizeof(char), NULL);
+
+	// 關閉與Clinet的聯繫
+	shutdown(clientSock, 0);
+	closesocket(clientSock);
+
+	// 關閉Server
+	closesocket(s);
+	WSACleanup();
+	system("pause");
+	return 0;
+}
